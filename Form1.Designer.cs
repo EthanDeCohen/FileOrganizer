a65c0be32f761cc0d3158f9acd0e7d753b8804b6
EthanDeCohen <e.decohen00@gmail.com>
2019-02-19 14:29:27 -0500

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace FileNameOrganizer
+
+
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.MakeChange = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this._BackButton = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.OpenCurrentDirectory = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MakeChange
+            // 
+            this.MakeChange.Location = new System.Drawing.Point(69, 218);
+            this.MakeChange.Margin = new System.Windows.Forms.Padding(2);
+            this.MakeChange.Name = "MakeChange";
+            this.MakeChange.Size = new System.Drawing.Size(326, 21);
+            this.MakeChange.TabIndex = 0;
+            this.MakeChange.Text = "Make Change";
+            this.MakeChange.UseVisualStyleBackColor = true;
+            this.MakeChange.Click += new System.EventHandler(this.MakeChange_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(180, 66);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(236, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox1.Location = new System.Drawing.Point(180, 126);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(85, 126);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(73, 13);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "Future Name";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(88, 94);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(70, 13);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "File Type";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(180, 94);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(236, 21);
+            this.comboBox2.TabIndex = 7;
+            this.comboBox2.DropDown += new System.EventHandler(this.comboBox2_DropDown);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Location = new System.Drawing.Point(85, 66);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(83, 13);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.Text = "File Destination";
+            // 
+            // _BackButton
+            // 
+            this._BackButton.Location = new System.Drawing.Point(11, 32);
+            this._BackButton.Margin = new System.Windows.Forms.Padding(2);
+            this._BackButton.Name = "_BackButton";
+            this._BackButton.Size = new System.Drawing.Size(62, 20);
+            this._BackButton.TabIndex = 10;
+            this._BackButton.Text = "Back";
+            this._BackButton.UseVisualStyleBackColor = true;
+            this._BackButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Location = new System.Drawing.Point(442, 6);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(91, 13);
+            this.textBox5.TabIndex = 11;
+            this.textBox5.Text = "Current Directory:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Location = new System.Drawing.Point(442, 25);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(290, 13);
+            this.textBox6.TabIndex = 12;
+            // 
+            // OpenCurrentDirectory
+            // 
+            this.OpenCurrentDirectory.Location = new System.Drawing.Point(442, 203);
+            this.OpenCurrentDirectory.Name = "OpenCurrentDirectory";
+            this.OpenCurrentDirectory.Size = new System.Drawing.Size(91, 37);
+            this.OpenCurrentDirectory.TabIndex = 13;
+            this.OpenCurrentDirectory.Text = "Open Current Directory";
+            this.OpenCurrentDirectory.UseVisualStyleBackColor = true;
+            this.OpenCurrentDirectory.Click += new System.EventHandler(this.OpenCurrentDirectory_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(442, 43);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(266, 139);
+            this.checkedListBox1.TabIndex = 14;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged_1);
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(633, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 36);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Select All";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.OpenCurrentDirectory);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this._BackButton);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.MakeChange);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.95D;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Ethan\'s Bad-Ass File Organizer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button MakeChange;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button _BackButton;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button OpenCurrentDirectory;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Diagnostics.EventLog eventLog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace FileNameOrganizer
+{
+    public partial class Form1 : Form
+    {
+        System.String[] CurrentDir = null;
+        System.String previousDir = null;
+        System.String currentDirPath = null;
+        List<string> listboxfiles = new List<string>();
+        protected System.String[] TopDir = System.IO.Directory.GetLogicalDrives();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] Root = System.IO.Directory.GetLogicalDrives();
+            this.comboBox1.Items.AddRange(Root);
+            this.textBox6.Text = "ROOT";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            PopulateCheckBox();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //This Button is design to navigate backwards.
+            this.listboxfiles.Clear();
+            this.comboBox1.Items.Clear();  //Clear both Combobox and the Listbox
+            this.checkedListBox1.Items.Clear();
+            try //Try to navigate backwards
+            {
+
+
+                if (this.currentDirPath == "C:\\" || this.currentDirPath.Length <= 4 || this.previousDir == "ROOT") //If your are currently at the Top of the File Tree
+                {
+                    string[] Root = System.IO.Directory.GetLogicalDrives();  //You can only navigate back to the Drives
+                    this.comboBox1.Items.AddRange(Root);                    //Populate Combobox with Directories
+                    this.textBox6.Text = "ROOT";                            //Tell User that you're in the root
+                    this.CurrentDir = Root;                                 //Current Directory update
+                    return;
+                }
+                this.currentDirPath = this.previousDir;
+                string[] DirFiles = System.IO.Directory.GetFiles(this.previousDir);         //Grab files for previous Directory
+                string[] Cdirectories = System.IO.Directory.GetDirectories(this.previousDir);
+                this.CurrentDir = System.IO.Directory.GetDirectories(this.previousDir);     //Grab final destination (Current)
+                this.comboBox1.Items.AddRange(this.CurrentDir);                             //Populate Combo and...
+                this.checkedListBox1.Items.AddRange(DirFiles);                                     //...list box
+                Check_Or_Uncheck_All();//Check the List.
+                this.listboxfiles.AddRange(DirFiles);
+                this.textBox6.Text = this.previousDir;                                      //Tell user where we're at
+
+
+                if ((this.previousDir.Length >= 4) && this.previousDir != "ROOT")//If previous Directory Isn't the top of the tree then...
+                    this.previousDir = System.IO.Directory.GetParent(this.previousDir).ToString();               //... update previous directory.
+                if (this.currentDirPath.Length <= 4)
+                {
+
+                    this.comboBox1.Items.Clear();  //Clear both Combobox and the Listbox
+                    this.checkedListBox1.Items.Clear();
+
+                    this.previousDir = "ROOT";
+                    this.comboBox1.Items.AddRange(Cdirectories);
+                    this.checkedListBox1.Items.AddRange(DirFiles);
+                    Check_Or_Uncheck_All();//Check the List.
+                    this.listboxfiles.AddRange(DirFiles);
+                    this.textBox6.Text = this.currentDirPath;
+
+                }
+            }
+            catch (System.Exception) //You're at the top of the File Tree
+            {
+                this.comboBox1.Items.Clear(); //Clear out the Combo-Box
+                this.comboBox1.Items.AddRange(this.TopDir); //Populate Combo box with Directories
+                this.textBox6.Text = "ROOT"; // Tell User where you are
+                MessageBox.Show("You cannot go back any futher"); //Stop trying to navigate backwards. You cannot!
+                this.checkedListBox1.Items.Clear(); //Clear everything Just it case.
+            }
+        }
+
+        private void comboBox1_DropDownClosed(object sender, EventArgs e)
+        {
+            this.comboBox1.Text = this.textBox6.Text;
+        }
+
+        public void GetFileTypes(String s, params string[] values)
+        {
+
+            if (!string.IsNullOrEmpty(s) || values.Length > 0)
+            {
+                foreach (string value in values)
+                {
+                    if (s.Contains(value))
+                        return;
+                }
+            }
+        }
+
+        private void DoesTypeExist(string[] file)
+        {
+            List<String> Types = new List<string>();
+            foreach (string extension in file)
+            {
+                if (!Types.Contains(System.IO.Path.GetExtension(extension)))
+                    Types.Add(System.IO.Path.GetExtension(extension));
+            }
+            this.comboBox2.Items.AddRange(Types.ToArray());
+        }
+
+        private void comboBox2_DropDown(object sender, EventArgs e)
+        {
+            this.comboBox2.Items.Clear();
+            try
+            {
+                DoesTypeExist(this.listboxfiles.ToArray());
+            }
+            catch (System.Exception)
+            {
+                MessageBox.Show("There are no available Files");
+            }
+        }
+
+        private void MakeChange_Click(object sender, EventArgs e)
+        {            
+            int i = 1;
+            if (this.comboBox2.Text != null)
+            {
+                try
+                {
+                    var FilesToQuery = this.listboxfiles.ToArray();
+                    int count= stringMatches(FilesToQuery, 
+                            this.textBox1.Text,
+                        System.IO.Path.GetExtension(
+                            this.comboBox2.Text));//Count how many occurences of file name that exists already
+                    for (i = 0; i <= (this.checkedListBox1.Items.Count - 1); i++)
+                    {   
+                        if (this.checkedListBox1.GetItemChecked(i))
+                        {
+                            if (System.IO.Path.GetExtension(this.checkedListBox1.Items[i].ToString()) == this.comboBox2.Text.ToString())
+                            {
+                                if (count>0)
+                                {
+                                    count++;
+                                    System.IO.File.Move(this.checkedListBox1.Items[i].ToString(), this.currentDirPath + "\\" + this.textBox1.Text + count +
+                                    System.IO.Path.GetExtension(this.checkedListBox1.Items[i].ToString()));
+                                }
+                                else
+                                {
+
+                                System.IO.File.Move(this.checkedListBox1.Items[i].ToString(), this.currentDirPath + "\\" + this.textBox1.Text +
+                                    System.IO.Path.GetExtension(this.checkedListBox1.Items[i].ToString()));
+                                    count++;
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (System.Exception)
+                {
+                    MessageBox.Show("Unable to change File name");
+                    return;
+                }
+                this.checkedListBox1.Items.Clear();
+                string[] newDirectories = System.IO.Directory.GetDirectories(this.currentDirPath); //Look inside the directory and save its children directories
+                string[] newfiles = System.IO.Directory.GetFiles(this.currentDirPath);             //Look inside the directory and save its member files
+
+                this.CurrentDir = newDirectories;                                                   //Save the current child directories to keep track of progress
+                this.comboBox1.Items.AddRange(newDirectories);                        //Populate the Combox with Child Directories
+                this.checkedListBox1.Items.AddRange(newfiles);                               //Populate the Listbox with child files.
+                Check_Or_Uncheck_All();//Check the List.
+                this.listboxfiles.AddRange(newfiles);
+
+            }
+        }
+        
+        private void OpenCurrentDirectory_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Clear();
+            OpenFileDialog x = new OpenFileDialog();
+            x.InitialDirectory = this.currentDirPath;
+            x.Multiselect = true;
+            x.ShowDialog();
+            string[] result = x.FileNames;
+            checkedListBox1.Items.AddRange(result);
+            SelectAll();
+                
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void PopulateCheckBox()
+        {
+            System.String newDir = this.comboBox1.Text; //This line saves the directory that the user has chosen.
+            this.listboxfiles.Clear();
+            this.textBox6.Text = this.comboBox1.Text; //This updates the Current directory Text box 
+            this.comboBox1.Items.Clear();             //This clears the Combobox
+            this.checkedListBox1.Items.Clear();              //This clears the listbox with the files we're interested in.
+            try //Try to gain access to files
+            {
+                string[] newDirectories = System.IO.Directory.GetDirectories(newDir); //Look inside the directory and save its children directories
+                 string[] newfiles = System.IO.Directory.GetFiles(newDir);             //Look inside the directory and save its member files
+
+                this.CurrentDir = newDirectories;                                     //Save the current child directories to keep track of progress
+                this.comboBox1.Items.AddRange(newDirectories);                        //Populate the Combox with Child Directories
+                this.checkedListBox1.Items.AddRange(newfiles);                               //Populate the Listbox with child files.
+                //Check_Or_Uncheck_All();//Check the List.
+                this.listboxfiles.AddRange(newfiles);
+
+                this.currentDirPath = this.textBox6.Text;                             //Update current Location
+            }
+            catch (System.UnauthorizedAccessException) //Access Denied to directory. 
+            {
+                MessageBox.Show("You are Unauthorized to Access this File");
+            }
+
+            try //If a previous Directory Exists, Try to assign it.
+            {
+                this.previousDir = System.IO.Directory.GetParent(this.currentDirPath).ToString();      //Update previous location
+            }
+            catch (System.Exception) //Previous directory doesn't exist. Assign root.
+            {
+                this.previousDir = "ROOT";
+            }
+            //this.checkedListBox1.Items.AddRange(this.listboxfiles.ToArray());
+        }
+
+        private void Check_Or_Uncheck_All()
+        { 
+            try
+            {   if (this.checkedListBox1.Items.Count <= 0) return;
+                foreach (object item in this.checkedListBox1.Items)
+                {
+                    ;
+                }
+            }catch(System.Exception)
+            {
+                MessageBox.Show("Something Went Wrong");
+            }
+        }
+
+        private void WhichItemsAreChecked()
+        {
+            int i;
+            string s;
+            s = "Checked items:\n";
+            for (i = 0; i <= (checkedListBox1.Items.Count - 1); i++)
+            {
+                if (checkedListBox1.GetItemChecked(i))
+                {
+                    s = s + "Item " + (i + 1).ToString() + " = " + checkedListBox1.Items[i].ToString() + "\n";
+                }
+            }
+            MessageBox.Show(s);
+        }
+
+        private void checkedListBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            SelectAll();
+        }
+
+        private void SelectAll()
+        {
+            int i;
+            string s;
+            s = "Checked items:\n";
+            for (i = 0; i <= (checkedListBox1.Items.Count - 1); i++)
+            {
+                if (!checkedListBox1.GetItemChecked(i))
+                {
+                    checkedListBox1.SetItemChecked(i, true);
+                    //s = s + "Item " + (i + 1).ToString() + " = " + checkedListBox1.Items[i].ToString() + "\n";
+                }
+            }
+        }
+
+        private int stringMatches(string[] textToQuery, String FileName,string ext)
+        {
+            int count = 0;
+            Regex Copy = new Regex(FileName.Split('.')[0] + @"\(\d+\)" + ext);
+            Regex Copy1 = new Regex(FileName.Split('.')[0] + @"\d+" + ext);
+            Regex Original = new Regex(FileName.Split('.')[0] + ext);
+            foreach(string file in textToQuery)
+            {
+                var origincheck = Original.Match(file);
+                var NumericalCheck = Copy1.Match(file);
+                var ParentheticalCheck = Copy.Match(file);
+                if (ParentheticalCheck.Success|| origincheck.Success ||NumericalCheck.Success)
+                    count++;                
+            }
+            return count;
+        }
+    }
+}
